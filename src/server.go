1b9496c10ec80c167b8025017e0cc524c63c6eb1
richtr <rich.tibbett@gmail.com>
2014-05-16 10:42:41 +0200

    Clean up http test console layout and expose only to localhost

--- src/server.go
+++ src/server.go
@@ -10,12 +10,11 @@ import (
 	"strconv"
 )
 
-var homeTempl = template.Must(template.ParseFiles("_www/testclient.html"))
-var scriptTempl = template.Must(template.ParseFiles("_www/_namedwebsockets.js"))
-
 var ValidServiceName = regexp.MustCompile("^[A-Za-z0-9_-]{1,255}$")
 
 var namedWebSockets = map[string]*NamedWebSocket{}
+
+var consoleTempl = template.Must(template.ParseFiles("_www/console.html"))
 
 func setupHTTP() {
 	name, err := os.Hostname()
@@ -25,35 +24,50 @@ func setupHTTP() {
 	}
 	LocalHost = name
 
-	http.HandleFunc("/", serveHTTP)
-	http.HandleFunc("/_namedwebsockets.js", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
-		scriptTempl.Execute(w, LocalPort)
-  })
+	// Serve the test console
+	http.HandleFunc("/", serveConsoleTemplate)
 
+	// Serve the web socket creation endpoints
 	http.HandleFunc("/local/", serveWSCreator)
 	http.HandleFunc("/broadcast/", serveWSCreator)
 
 	portStr := strconv.Itoa(LocalPort)
 
+	// Bind and serve on device's public interface
 	go http.ListenAndServe(LocalHost + ":" + portStr, nil)
 
-	// Bind and serve also on loopback address
+	// Bind and serve also on device's loopback address
 	http.ListenAndServe("localhost:" + portStr, nil)
 }
 
-func serveHTTP(w http.ResponseWriter, r *http.Request) {
+func serveConsoleTemplate(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	portSuffix := ":" + strconv.Itoa(LocalPort)
+
+	// Only allow access from localhost
+	if r.Host != "localhost" + portSuffix && r.Host != "127.0.0.1" + portSuffix {
+		http.Error(w, "Permission denied. Named WebSockets Test Console is only accessible from localhost", 403)
+		return
+	}
+
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
-	if r.URL.Path != "/" {
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h2>Named WebSockets Proxy is running!</h2>")
+		return
+	}
+
+	if r.URL.Path != "/console" {
 		http.Error(w, "Not found", 404)
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTempl.Execute(w, r.Host)
+	consoleTempl.Execute(w, LocalPort)
 }
 
 func serveWSCreator(w http.ResponseWriter, r *http.Request) {
