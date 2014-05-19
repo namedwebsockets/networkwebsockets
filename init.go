aa1285c68d63188f73fd140cc5fff8c96429e3bf
richtr <rich.tibbett@gmail.com>
2014-05-19 10:51:09 +0200

    Move host/port variables to server file

--- init.go
+++ init.go
@@ -1,13 +1,9 @@
 package main
 
-// Configured at runtime
-var LocalHost = "localhost"
-var LocalPort = 9009
-
 func main() {
 	// Start Bonjour discovery service
 	NewDiscoveryServer()
 
 	// Start HTTP/WebSocket endpoint server
-	setupHTTP()
+	SetupHTTP()
 }
